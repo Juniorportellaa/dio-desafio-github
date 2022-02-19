561a04c5fa3a256a882882500a6e49adf638c0d8
Junior <juniorportellaa@gmail.com>
2022-02-19 15:34:16 -0300

    Primeiros P. NET

---
+++ VSCode/ExemploTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    int numeroderepetição = 6;
+
+    for (int i=1; i < numeroderepetição; i++)
+    {
+        Console.WriteLine($"Bem-vindo ao curso de .NET {i}" );
+    };
